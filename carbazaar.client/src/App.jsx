81a497d75793ab240d3d9393a71b347bc6f4a1e5
BojidarS <pichbg124@gmail.com>
2024-11-19 10:36:50 +0200

    Mui Installed

--- carbazaar.client/src/App.jsx
+++ carbazaar.client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from '@mui/material';
 
 const App = () => {
     return(
         <div>
           <h1>Welcome to CarBazaar</h1>
+          <Button variant='contained' color='primary'>MUI Installed</Button>
         </div>
     );
 };

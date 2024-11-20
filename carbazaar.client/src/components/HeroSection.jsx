3a6ae102366856e5a6908dee465d585bf403a4c3
BojidarS <pichbg124@gmail.com>
2024-11-20 18:59:25 +0200

    Fixed HeroSection, added basic search

--- carbazaar.client/src/components/HeroSection.jsx
+++ carbazaar.client/src/components/HeroSection.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
-import { Box, Typography, Button, TextField, Grid2 } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Typography, Button, InputLabel, Grid2, FormControl, MenuItem, Select } from '@mui/material';
 
 const HeroSection = () => {
+    const [carType, setCarType] = useState('All');
+    const [carBrand, setCarBrand] = useState('All');
+    const [priceRange, setPriceRange] = useState('All');
+
+    const handleCarTypeChange = (e) => setCarType(e.target.value);
+    const handleCarBrandChange = (e) => setCarBrand(e.target.value);
+    const handlePriceRangeChange = (e) => setPriceRange(e.target.value);
+    
+    const handleSearch = () => {
+        console.log(carType, carBrand, priceRange);
+    };
+
     return (
         <Box sx={{
             display: 'flex',
@@ -23,22 +35,49 @@ const HeroSection = () => {
 
             {/* Search Bar */}
             <Grid2 container spacing={2} justifyContent='center' sx={{ mb: 4, width: '100%' }}>
-                <Grid2 sx={{ display: 'flex', flexGrow: 0.3}}>
-                    <TextField
-                    fullWidth
-                    placeholder='Search for cars...'
-                    variant='outlined'
-                    size='medium'
-                    sx={{ backgroundColor: 'background.paper' }}
-                    />
+                {/* Car Type Filter */}
+                <Grid2 sx={{display: 'flex', flexGrow: 0.05}}>
+                    <FormControl fullWidth>
+                        <InputLabel>Car Type</InputLabel>
+                        <Select value={carType} onChange={handleCarTypeChange} label='Car Type'>
+                            <MenuItem value='All'>All</MenuItem>
+                            <MenuItem value='SUV'>SUV</MenuItem>
+                            <MenuItem value='Sedan'>Sedan</MenuItem>
+                            <MenuItem value='Truck'>Truck</MenuItem>
+                            <MenuItem value='Convertible'>Convertible</MenuItem>
+                        </Select>
+                    </FormControl>
                 </Grid2>
+
+                {/* Car Brand Filter */}
+                <Grid2 sx={{display: 'flex', flexGrow: 0.05}}>
+                    <FormControl fullWidth>
+                        <InputLabel>Car Brand</InputLabel>
+                        <Select value={carBrand} onChange={handleCarBrandChange} label='Car Brand'>
+                            <MenuItem value='All'>All</MenuItem>
+                            <MenuItem value='Mercedes'>Mercedes</MenuItem>
+                            <MenuItem value='BMW'>BMW</MenuItem>
+                            <MenuItem value='Audi'>Audi</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid2>
+
+                {/* Price Range Filter */}
+                <Grid2 sx={{display: 'flex', flexGrow: 0.05}}>
+                    <FormControl fullWidth>
+                        <InputLabel>Price Range</InputLabel>
+                        <Select value={priceRange} onChange={handlePriceRangeChange} label='Price Range'>
+                            <MenuItem value='All'>All</MenuItem>
+                            <MenuItem value='0-10000'>0 - 10,000</MenuItem>
+                            <MenuItem value='10000-30000'>10,000 - 30,000</MenuItem>
+                            <MenuItem value='50000+'>50,000+</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid2>
+
+                {/* Search Button */}
                 <Grid2>
-                    <Button
-                    variant='contained'
-                    color='primary'
-                    size='large'
-                    sx={{ height: '100%' }}
-                    >
+                    <Button variant='contained' color='primary' size='large' onClick={handleSearch} sx={{ height: '100%', borderRadius: 2 }}>
                         Search
                     </Button>
                 </Grid2>

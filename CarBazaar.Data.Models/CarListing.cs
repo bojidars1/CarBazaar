4ff3af683afdd9cfb5478a1bc4dff50cf6352d03
BojidarS <pichbg124@gmail.com>
2024-11-24 08:37:17 +0200

    Added CarListing Entity

---
+++ CarBazaar.Data.Models/CarListing.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarBazaar.Data.Models
+{
+    public class CarListing
+    {
+        [Key]
+        [Comment("Car Listing Identifier")]
+        public int Id { get; set; }
+
+        [Comment("Car Listing Name")]
+        public string Name { get; set; } = null!;
+
+        [Comment("Car Type")]
+        public string Type { get; set; } = null!;
+
+        [Comment("Car Brand")]
+        public string Brand { get; set; } = null!;
+
+        [Comment("Car Price")]
+        public decimal Price { get; set; }
+
+        [Comment("Car Gearbox")]
+        public string Gearbox { get; set; } = null!;
+
+        [Comment("Car State")]
+        public string State { get; set; } = null!;
+
+        [Comment("Car KM")]
+        public long Km { get; set; }
+
+        [Comment("Car Production Date")]
+        public DateTime ProductionDate { get; set; }
+
+        [Comment("Car Horsepower")]
+        public int Horsepower { get; set; }
+
+        [Comment("Car Color")]
+        public string Color { get; set; } = null!;
+
+        [Comment("Extra Car Info")]
+        public string ExtraInfo { get; set; } = null!;
+
+        [Comment("Listing Publication Date")]
+        public DateTime PublicationDate { get; set; }
+    }
+}
--- CarBazaar.Data/CarBazaarDbContext.cs
+++ CarBazaar.Data/CarBazaarDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CarBazaar.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,7 @@ namespace CarBazaar.Data
     public class CarBazaarDbContext : DbContext
     {
         public CarBazaarDbContext(DbContextOptions<CarBazaarDbContext> options) : base(options) { }
+
+        public DbSet<CarListing> CarListings { get; set; }
     }
 }

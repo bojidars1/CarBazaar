170839b5efd65810e6764bcfedf1946b544b2fd2
BojidarS <pichbg124@gmail.com>
2024-11-24 08:46:11 +0200

    Added Db in IO Container

--- CarBazaar.Server/Program.cs
+++ CarBazaar.Server/Program.cs
@@ -1,7 +1,14 @@
+using CarBazaar.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<CarBazaarDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CarBazaarDbContext"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

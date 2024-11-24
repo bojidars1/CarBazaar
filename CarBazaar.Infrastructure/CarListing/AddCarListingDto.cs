395eee0352c5a7b6c497d3e08694b94a8bce10f3
BojidarS <pichbg124@gmail.com>
2024-11-24 09:56:39 +0200

    Added CarListingController and AddCarListingDto

---
+++ CarBazaar.Infrastructure/CarListing/AddCarListingDto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.Infrastructure.CarListing
+{
+	public class AddCarListingDto
+	{
+		[Required]
+		public string Name { get; set; } = null!;
+
+		[Required]
+		public string Type { get; set; } = null!;
+
+		[Required]
+		public string Brand { get; set; } = null!;
+
+		[Required]
+		public decimal Price { get; set; }
+
+		[Required]
+		public string Gearbox { get; set; } = null!;
+
+		[Required]
+		public string State { get; set; } = null!;
+
+		[Required]
+		public long Km { get; set; }
+
+		[Required]
+		public DateTime ProductionDate { get; set; }
+
+		[Required]
+		public int Horsepower { get; set; }
+
+		[Required]
+		public string Color { get; set; } = null!;
+
+		[Required]
+		public string ExtraInfo { get; set; } = null!;
+	}
+}
---
+++ CarBazaar.Server/Controllers/CarListingController.cs
@@ -0,0 +1,43 @@
+﻿using CarBazaar.Data;
+using CarBazaar.Data.Models;
+using CarBazaar.Infrastructure.CarListing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBazaar.Server.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CarListingController(CarBazaarDbContext context) : Controller
+	{
+		[HttpPost]
+		public async Task<IActionResult> Add(AddCarListingDto dto)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			CarListing carListing = new CarListing
+			{
+				Id = Guid.NewGuid(),
+				Name = dto.Name,
+				Type = dto.Type,
+				Brand = dto.Brand,
+				Price = dto.Price,
+				Gearbox = dto.Gearbox,
+				State = dto.State,
+				Km = dto.Km,
+				ProductionDate = dto.ProductionDate,
+				Horsepower = dto.Horsepower,
+				Color = dto.Color,
+				ExtraInfo = dto.ExtraInfo,
+				PublicationDate = DateTime.Now
+			};
+
+			context.CarListings.Add(carListing);
+			await context.SaveChangesAsync();
+
+			return Ok(carListing);
+		}
+	}
+}

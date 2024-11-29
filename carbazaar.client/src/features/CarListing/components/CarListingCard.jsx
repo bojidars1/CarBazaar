d1fb6921e27794ba16ef6e26ad0750b0734f04cc
BojidarS <pichbg124@gmail.com>
2024-11-29 17:41:23 +0200

    Added CarListingCard and CarListings components

---
+++ carbazaar.client/src/features/CarListing/components/CarListingCard.jsx
@@ -0,0 +1,41 @@
+import { Card, CardContent, CardMedia, Typography, Box } from '@mui/material';
+import React from 'react';
+
+const CarListingCard = ({ car, onDetailsClick }) => {
+    return (
+        <Card sx={{ maxWidth: 345, margin: 2, boxShadow: 3 }}>
+            {/* Car Image */}
+            <CardMedia
+            component={img}
+            height='200'
+            image={car.imageURL || 'https://www.ilusso.com/wp-content/uploads/3-3-1024x683.jpg'}
+            alt={car.name}
+            />
+
+            {/* Car Details */}
+            <CardContent>
+                <Typography variant='h6' component="div" sx={{ fontWeight: 'bold' }}>
+                    {car.name}
+                </Typography>
+
+                <Typography variant='body2' color='text.secondary'>
+                    Price: ${car.price}
+                </Typography>
+            </CardContent>
+
+            {/* CTA Button */}
+            <Box sx={{ p: 2 }}>
+                <Button
+                variant='contained'
+                color='primary'
+                fullWidth
+                onClick={() => onDetailsClick(car.id)}
+                >
+                    View Details
+                </Button>
+            </Box>
+        </Card>
+    );
+};
+
+export default CarListingCard;
---
+++ carbazaar.client/src/features/CarListing/components/CarListings.jsx
@@ -0,0 +1,58 @@
+import { Box, CircularProgress, Typography } from '@mui/material';
+import axios from 'axios';
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import CarListingCard from './CarListingCard';
+
+const CarListings = () => {
+    const [carListings, setCarListings] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        const getCarListings = async () => {
+            try {
+                const response = await axios.get('https://localhost:7100/api/CarListing');
+                setCarListings(response.data);
+            } catch (err) {
+                setError("Failed to get car listings. Please try again.")
+            } finally {
+                setLoading(false);
+            }
+        };
+        
+        getCarListings();
+    }, []);
+
+    const handleDetailsClick = (id) => {
+        console.log("View details for this car listing.");
+    };
+
+    return (
+        <Box sx={{ p: 2 }}>
+            <Typography variant='h4' sx={{ textAlign: 'center', mb: 3}}>
+                Car Listings
+            </Typography>
+            
+            {loading ? (
+                <Box sx={{ display: 'flex', justifyContent: 'center', mt: 5 }}>
+                    <CircularProgress />
+                </Box>
+            ) : error ? (
+                <Typography color='error' variant='h6' sx={{ textAlign: 'center' }}>
+                    {error}
+                </Typography>
+            ) : (
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}
+                >
+                    {carListings.map((car) => (
+                        <CarListingCard key={car.id} car={car} onDetailsClick={handleDetailsClick} />
+                    ))}
+                </Box>
+            )}
+        </Box>
+    );
+};
+
+export default CarListings;

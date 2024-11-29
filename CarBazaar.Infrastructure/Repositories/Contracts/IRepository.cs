9ff0479b193a3a044fa92c2d28d3c8a6dd9a2142
BojidarS <pichbg124@gmail.com>
2024-11-29 23:22:11 +0200

    Added IRepository<T> and base Repository

---
+++ CarBazaar.Infrastructure/Repositories/Contracts/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.Infrastructure.Repositories.Contracts
+{
+	public interface IRepository<T> where T : class
+	{
+		Task<List<T>> GetAllAsync();
+		
+		Task<T?> GetByIdAsync(string id);
+
+		Task AddAsync(T entity);
+
+		Task UpdateAsync(T entity);
+
+		Task DeleteByIdAsync(string id);
+	}
+}

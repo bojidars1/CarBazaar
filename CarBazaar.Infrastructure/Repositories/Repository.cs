28726521aa6cc09ac1d2ff1432aa1ff6ebf70488
BojidarS <pichbg124@gmail.com>
2024-11-29 23:25:17 +0200

    Made ctor of Repository

--- CarBazaar.Infrastructure/Repositories/Repository.cs
+++ CarBazaar.Infrastructure/Repositories/Repository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CarBazaar.Data;
+using CarBazaar.Infrastructure.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,40 @@ using System.Threading.Tasks;
 
 namespace CarBazaar.Infrastructure.Repositories
 {
-	public class Repository
+	public class Repository<T> : IRepository<T> where T : class
 	{
+		private readonly CarBazaarDbContext _context;
+		private readonly DbSet<T> _dbSet;
+
+        public Repository(CarBazaarDbContext context)
+        {
+            _context = context;
+			_dbSet = context.Set<T>();
+        }
+
+        public Task AddAsync(T entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task DeleteByIdAsync(string id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<T>> GetAllAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<T?> GetByIdAsync(string id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task UpdateAsync(T entity)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }

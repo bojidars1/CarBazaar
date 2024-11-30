8fb416de1fba0db8070d0a2de8c5ba72a6bb27dd
BojidarS <pichbg124@gmail.com>
2024-12-01 01:56:14 +0200

    Added ICarListingService

---
+++ CarBazaar.Services/Contracts/ICarListingService.cs
@@ -0,0 +1,17 @@
+﻿using CarBazaar.ViewModels.CarListing;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.Services.Contracts
+{
+	public interface ICarListingService
+	{
+		public Task AddAsync([FromBody]AddCarListingDto dto);
+		
+		public Task<List<CarListingListDetailsDto>> GetAllAsync();
+	}
+}

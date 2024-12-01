2ed996591fa4bc4aac31a171ad7ffb4d3959c07c
BojidarS <pichbg124@gmail.com>
2024-12-01 15:33:01 +0200

    Updated Delete View

--- carbazaar.client/src/features/CarListing/components/DeleteCarListing.jsx
+++ carbazaar.client/src/features/CarListing/components/DeleteCarListing.jsx
@@ -1,15 +1,36 @@
-import { Button, Paper, Typography } from '@mui/material';
+import { Button, Paper, Typography, Box } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const DeleteCarListing = () => {
     const { id } = useParams();
+    
 
     return (
-    <Paper elevation={3}>
-        <Typography variant='h3' color='error'>Delete Car Listing: ?</Typography>
-        <Button variant='contained' color='error'>Delete</Button>
-        <Button variant='outlined' color='secondary'>Back to Listings</Button>
+    <Paper elevation={6} sx={{
+        p: 4,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        maxWidth: '80%',
+        margin: 'auto',
+        borderRadius: 2,
+        boxShadow: 3
+    }}>
+        <Typography variant='h4' color='error'>Delete Car Listing: <strong>Listing</strong>?</Typography>
+        <Box sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            width: '100%',
+            mt: 5
+        }}>
+            <Button variant='outlined' color='secondary' sx={{ width: '40%' }}>
+                Back to Listings
+            </Button>
+            <Button variant='contained' color='error' sx={{ width: '40%' }}>
+                Delete
+            </Button>
+        </Box>
     </Paper>
     );
 };

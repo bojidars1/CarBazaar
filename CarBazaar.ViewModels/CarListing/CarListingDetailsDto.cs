ea70aba6f5ff9011473b0bd19b63f697386364bb
BojidarS <pichbg124@gmail.com>
2024-12-01 02:15:54 +0200

    Added CarListingDetailsDto

---
+++ CarBazaar.ViewModels/CarListing/CarListingDetailsDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.ViewModels.CarListing
+{
+    public class CarListingDetailsDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Type { get; set; } = null!;
+
+        public string Brand { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public string Gearbox { get; set; } = null!;
+
+        public string State { get; set; } = null!;
+
+        public long KM { get; set; }
+
+        public int ProductionYear { get; set; }
+
+        public int Horsepower { get; set; }
+
+        public string Color { get; set; } = null!;
+
+        public string ExtraInfo { get; set; } = string.Empty;
+
+        public string ImageURL { get; set; } = string.Empty;
+    }
+}

4f12d1e3240ec851da373cce1c96eba786a5572b
BojidarS <pichbg124@gmail.com>
2024-12-01 17:38:16 +0200

    Added CarListingSearchDto

---
+++ CarBazaar.ViewModels/CarListing/CarListingSearchDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.ViewModels.CarListing
+{
+	public class CarListingSearchDto
+	{
+		public string Type { get; set; } = null!;
+
+		public string Brand { get; set; } = null!;
+
+		public string PriceRange { get; set; } = null!;
+	}
+}

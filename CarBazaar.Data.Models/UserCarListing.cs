38f6d1bed5a28f91cbec67940ff36e133d00f08a
BojidarS <pichbg124@gmail.com>
2024-12-08 23:05:57 +0200

    Added UserCarListing Entity

---
+++ CarBazaar.Data.Models/UserCarListing.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.Data.Models
+{
+	public class UserCarListing
+	{
+		[Comment("User Id")]
+		public string UserId { get; set; } = null!;
+
+		[ForeignKey("UserId")]
+		[Comment("User")]
+		public CarBazaarUser User { get; set; } = new CarBazaarUser();
+
+		[Comment("Car Listing Id")]
+		public Guid CarListingId {  get; set; }
+
+		[ForeignKey("CarListingId")]
+		[Comment("Car Listing")]
+		public CarListing CarListing { get; set; } = new CarListing();
+	}
+}

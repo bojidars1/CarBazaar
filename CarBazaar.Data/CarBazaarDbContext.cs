96066880b4707f0a1c72c8b5235cc48f7a7f2636
BojidarS <pichbg124@gmail.com>
2024-12-08 23:12:58 +0200

    Setup relationship between users and carlistings

--- CarBazaar.Data/CarBazaarDbContext.cs
+++ CarBazaar.Data/CarBazaarDbContext.cs
@@ -15,9 +15,24 @@ namespace CarBazaar.Data
 
         public DbSet<CarListing> CarListings { get; set; }
 
+		public DbSet<UserCarListing> UserCarListings { get; set; }
+
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<UserCarListing>()
+				.HasKey(uc => new { uc.UserId, uc.CarListingId });
+
+			modelBuilder.Entity<UserCarListing>()
+				.HasOne(uc => uc.User)
+				.WithMany()
+				.HasForeignKey(uc => uc.UserId);
+
+			modelBuilder.Entity<UserCarListing>()
+				.HasOne(uc => uc.CarListing)
+				.WithMany()
+				.HasForeignKey(uc => uc.CarListingId);
 
 			modelBuilder.Entity<CarListing>().HasQueryFilter(cl => !cl.IsDeleted);
 		}
--- CarBazaar.Data/Migrations/20241208211234_Added_UserCarListing_Entity.Designer.cs
+++ CarBazaar.Data/Migrations/20241208211234_Added_UserCarListing_Entity.Designer.cs
@@ -4,6 +4,7 @@ using CarBazaar.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarBazaar.Data.Migrations
 {
     [DbContext(typeof(CarBazaarDbContext))]
-    partial class CarBazaarDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241208211234_Added_UserCarListing_Entity")]
+    partial class Added_UserCarListing_Entity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

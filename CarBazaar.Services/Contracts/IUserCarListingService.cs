0f681563e2e327a86b374af291aa28849da7031e
BojidarS <pichbg124@gmail.com>
2024-12-08 23:38:33 +0200

    Added UserCarListingService

---
+++ CarBazaar.Services/Contracts/IUserCarListingService.cs
@@ -0,0 +1,14 @@
+﻿using CarBazaar.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.Services.Contracts
+{
+    public interface IUserCarListingService
+    {
+        public Task AddAsync(UserCarListing listing);
+    }
+}
---
+++ CarBazaar.Services/UserCarListingService.cs
@@ -0,0 +1,19 @@
+﻿using CarBazaar.Data.Models;
+using CarBazaar.Infrastructure.Repositories.Contracts;
+using CarBazaar.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.Services
+{
+	public class UserCarListingService(IUserCarListingRepository repository) : IUserCarListingService
+	{
+		public async Task AddAsync(UserCarListing listing)
+		{
+			await repository.AddAsync(listing);
+		}
+	}
+}

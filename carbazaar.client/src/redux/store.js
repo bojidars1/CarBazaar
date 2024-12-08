25225ff1e602f2915a2551d27b7b4b48688a55c8
BojidarS <pichbg124@gmail.com>
2024-12-08 15:44:23 +0200

    Updated store

--- carbazaar.client/src/redux/store.js
+++ carbazaar.client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './userSlice';
+import authReducer from './authSlice';
 
 const store = configureStore({
     reducer: {
+        auth: authReducer,
         user: userReducer
     }
 });

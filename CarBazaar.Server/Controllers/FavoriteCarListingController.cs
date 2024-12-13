9f0aa3725b72e44f3c023fafd303bf2754b92e99
BojidarS <pichbg124@gmail.com>
2024-12-13 19:00:40 +0200

    Implemented GetFavourites Action

--- CarBazaar.Server/Controllers/FavoriteCarListingController.cs
+++ CarBazaar.Server/Controllers/FavoriteCarListingController.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CarBazaar.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarBazaar.Server.Controllers
 {
     [Authorize]
-    public class FavoriteCarListingController : BaseController
+    public class FavoriteCarListingController(IFavouriteCarListingService favouriteCarListingService) : BaseController
     {
-        
+        [HttpGet("get-favourites")]
+        public async Task<IActionResult> GetFavourites()
+        {
+            string? userId = GetUserId();
+            if (userId == null)
+            {
+                return BadRequest();
+            }
+
+            var favourites = await favouriteCarListingService.GetFavouritesAsync(userId);
+            return Ok(favourites);
+        }
     }
 }

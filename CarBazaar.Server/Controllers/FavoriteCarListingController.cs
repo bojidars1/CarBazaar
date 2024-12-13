3bafb358238f5f620d771041c6cd4d8e4a18228a
BojidarS <pichbg124@gmail.com>
2024-12-13 18:36:51 +0200

    Added FavoriteCarListingController

---
+++ CarBazaar.Server/Controllers/FavoriteCarListingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBazaar.Server.Controllers
+{
+    [Authorize]
+    public class FavoriteCarListingController : BaseController
+    {
+        
+    }
+}

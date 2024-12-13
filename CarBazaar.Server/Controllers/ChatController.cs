6249b0fe1b4a4afed1724a910afa1abf524fff7e
BojidarS <pichbg124@gmail.com>
2024-12-14 00:28:14 +0200

    Added ChatController

---
+++ CarBazaar.Server/Controllers/ChatController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBazaar.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[Authorize]
+	public class ChatController : BaseController
+	{
+		
+	}
+}

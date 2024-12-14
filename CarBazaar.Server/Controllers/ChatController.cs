a115a3763f41de17a67fdb8dfdffc8bc6237bcb9
BojidarS <pichbg124@gmail.com>
2024-12-14 13:37:51 +0200

    Implemented SendMessage Action

--- CarBazaar.Server/Controllers/ChatController.cs
+++ CarBazaar.Server/Controllers/ChatController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CarBazaar.Data;
+using CarBazaar.Data.Models;
+using CarBazaar.ViewModels.Chat;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,8 +9,29 @@ namespace CarBazaar.Server.Controllers
 {
 	[Route("api/[controller]")]
 	[Authorize]
-	public class ChatController : BaseController
+	public class ChatController(CarBazaarDbContext context) : BaseController
 	{
-		public async Task<IActionResult> SendMessage([FromBody] SendMessageRequest)
+		public async Task<IActionResult> SendMessage([FromBody] SendMessageRequest request)
+		{
+			var userId = GetUserId();
+			if (userId == null)
+			{
+				return BadRequest("User id not found");
+			}
+
+			var message = new ChatMessage
+			{
+				SenderId = userId,
+				ReceiverId = request.ReceiverId,
+				Message = request.Message,
+				CarListingId = request.CarListingId,
+				Timestamp = DateTime.UtcNow
+			};
+
+			await context.ChatMessages.AddAsync(message);
+			await context.SaveChangesAsync();
+
+			return Ok("Message sent");
+		}
 	}
 }

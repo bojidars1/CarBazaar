08f3ee2d2f31dd76b003cdcbded2be7320e11e3a
BojidarS <pichbg124@gmail.com>
2024-12-14 22:13:11 +0200

    Added ChatService

---
+++ CarBazaar.Services/ChatService.cs
@@ -0,0 +1,22 @@
+﻿using CarBazaar.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.Services
+{
+	public class ChatService : IChatService
+	{
+		public bool IsOneOfThemOwner(string userId, string receiverId, string carListingId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task SendMessageAsync(string userId, string receiverId, string carListingId, string message)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ CarBazaar.Services/Contracts/IChatService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.Services.Contracts
+{
+	public interface IChatService
+	{
+		Task SendMessageAsync(string userId, string receiverId, string carListingId, string message);
+
+		bool IsOneOfThemOwner(string userId, string receiverId, string carListingId);
+	}
+}

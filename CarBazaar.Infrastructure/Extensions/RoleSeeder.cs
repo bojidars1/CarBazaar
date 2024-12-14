5a1faca0aa8c4f943d84d24eee3be3bf362b11a8
BojidarS <pichbg124@gmail.com>
2024-12-14 23:35:22 +0200

    Added RoleSeeder

---
+++ CarBazaar.Infrastructure/Extensions/RoleSeeder.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.Infrastructure.Extensions
+{
+	public class RoleSeeder
+	{
+		public static async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
+		{
+			var roles = new[] { "User", "Administrator" };
+
+			foreach (var role in roles)
+			{
+				var roleExists = await roleManager.RoleExistsAsync(role);
+				if (!roleExists)
+				{
+					await roleManager.CreateAsync(new IdentityRole(role));
+				}
+			}
+		}
+	}
+}

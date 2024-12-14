8c57e9c5d6d1d8374fb16c8be1ce8fc194f10ddd
BojidarS <pichbg124@gmail.com>
2024-12-14 23:52:47 +0200

    Added Admin Controller

---
+++ CarBazaar.Server/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBazaar.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[Authorize(Roles = "Administrator")]
+	public class AdminController : BaseController
+	{
+		[HttpGet]
+		public IActionResult TestAdminRole()
+		{
+			return Ok("You are an admin");
+		}
+	}
+}

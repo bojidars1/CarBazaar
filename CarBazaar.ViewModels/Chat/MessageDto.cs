da2665e342849d5d97ee7d1c02447409425d1f6b
BojidarS <pichbg124@gmail.com>
2024-12-14 22:31:56 +0200

    Added MessageDto

---
+++ CarBazaar.ViewModels/Chat/MessageDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.ViewModels.Chat
+{
+	public class MessageDto
+	{
+		public string Message { get; set; } = null!;
+
+		public string SenderId { get; set; } = null!;
+
+		public string ParticipantId { get; set; } = null!;
+
+		public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+	}
+}

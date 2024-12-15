46bf08a336d03c39b575e2c83e0ccfdcb270646f
BojidarS <pichbg124@gmail.com>
2024-12-16 00:24:52 +0200

    Added UserInfoPaginatedDto

---
+++ CarBazaar.ViewModels/User/UserInfoPaginatedDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.ViewModels.User
+{
+	public class UserInfoPaginatedDto
+	{
+		public List<UserInfoDto> Items { get; set; } = new List<UserInfoDto>();
+
+		public int TotalPages { get; set; }
+	}
+}

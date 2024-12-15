fdf7e4278c47f69caf21c6e7fca955e4b62328b0
BojidarS <pichbg124@gmail.com>
2024-12-16 00:31:54 +0200

    Implemented GetUsersPaginated

--- CarBazaar.Server/Controllers/AdminController.cs
+++ CarBazaar.Server/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CarBazaar.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,12 +7,13 @@ namespace CarBazaar.Server.Controllers
 {
 	[Route("api/[controller]")]
 	[Authorize(Policy = "RequireAdministratorRole")]
-	public class AdminController : BaseController
+	public class AdminController(IUserService userService) : BaseController
 	{
 		[HttpGet("get-users")]
-		public async Task<IActionResult> GetUsers()
+		public async Task<IActionResult> GetUsersPaginated([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
 		{
-
+			var users = await userService.GetUserInfoPaginated(page, pageSize);
+			return Ok(users);
 		}
 	}
 }

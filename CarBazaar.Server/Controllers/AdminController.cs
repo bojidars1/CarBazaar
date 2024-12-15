b42dd1567bf87b15eebd26999c85b3cae2a27d2a
BojidarS <pichbg124@gmail.com>
2024-12-16 00:41:01 +0200

    Added GetListingsPaginated

--- CarBazaar.Server/Controllers/AdminController.cs
+++ CarBazaar.Server/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using CarBazaar.Services.Contracts;
+﻿using CarBazaar.Services;
+using CarBazaar.Services.Contracts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -7,13 +8,20 @@ namespace CarBazaar.Server.Controllers
 {
 	[Route("api/[controller]")]
 	[Authorize(Policy = "RequireAdministratorRole")]
-	public class AdminController(IUserService userService) : BaseController
+	public class AdminController(IUserService userService,ICarListingService carListingService) : BaseController
 	{
 		[HttpGet("get-users")]
 		public async Task<IActionResult> GetUsersPaginated([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
 		{
 			var users = await userService.GetUserInfoPaginated(page, pageSize);
 			return Ok(users);
+		}
+
+		[HttpGet("get-listings")]
+		public async Task<IActionResult> GetListingsPaginated([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+		{
+			var listings = await carListingService.GetPaginatedCarListingsAsync(page, pageSize);
+			return Ok(listings);
 		}
 	}
 }

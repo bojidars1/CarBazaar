2cc2c86dc6619ca5534f89e1b3e4568416b91983
BojidarS <pichbg124@gmail.com>
2024-12-16 15:24:49 +0200

    Implemented GetNotificationsAsync in NotificationService

--- CarBazaar.Services/NotificationService.cs
+++ CarBazaar.Services/NotificationService.cs
@@ -13,7 +13,24 @@ namespace CarBazaar.Services
 	{
 		public async Task<NotificationShowPaginatedDto> GetNotificationsAsync(string userId, int page, int pageSize)
 		{
+			var query = notificationRepository.GetBaseQuery();
+			query.Where(n => n.UserId == userId);
 
+			var userNotificationsPaginated = await notificationRepository.GetPaginatedAsync(page, pageSize, query);
+
+			int totalPages = userNotificationsPaginated.TotalPages;
+			var items = userNotificationsPaginated.Select(n => new NotificationShowDto
+			{
+				Id = n.Id.ToString(),
+				Message = n.Message,
+				isRead = n.isRead
+			}).ToList();
+
+			return new NotificationShowPaginatedDto
+			{
+				Items = items,
+				TotalPages = totalPages
+			};
 		}
 	}
 }

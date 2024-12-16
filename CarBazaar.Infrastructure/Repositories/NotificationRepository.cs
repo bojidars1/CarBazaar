763aa1e195dc6157db084ee7ede5cc2cd9fbd50d
BojidarS <pichbg124@gmail.com>
2024-12-16 17:32:33 +0200

    Implemented MarkAsReadAsync in NotificationRepository

--- CarBazaar.Infrastructure/Repositories/NotificationRepository.cs
+++ CarBazaar.Infrastructure/Repositories/NotificationRepository.cs
@@ -2,6 +2,7 @@
 using CarBazaar.Data.Models;
 using CarBazaar.Infrastructure.Extensions;
 using CarBazaar.Infrastructure.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,9 +21,14 @@ namespace CarBazaar.Infrastructure.Repositories
 			return await GetPaginatedAsync(page, pageSize, query);
 		}
 
-		//public Task MarkAsReadAsync(List<string> notificationsIds)
-		//{
-		//	throw new NotImplementedException();
-		//}
+		public async Task MarkAsReadAsync(List<Guid> notificationsIds)
+		{
+			var notifications = await context.Notifications
+				.Where(n => notificationsIds.Contains(n.Id))
+				.ToListAsync();
+
+			notifications.ForEach(n => n.isRead = true);
+			await context.SaveChangesAsync();
+		}
 	}
 }

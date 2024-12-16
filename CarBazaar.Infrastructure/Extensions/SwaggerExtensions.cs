0f03aa1b7aec318477baa1ad30572e3e15690343
BojidarS <pichbg124@gmail.com>
2024-12-16 22:21:15 +0200

    Readded Swagger Extensions

---
+++ CarBazaar.Infrastructure/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace CarBazaar.Infrastructure.Extensions
+{
+	public static class SwaggerExtensions
+	{
+		public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+		{
+			services.AddEndpointsApiExplorer();
+			services.AddSwaggerGen(options =>
+			{
+				options.SwaggerDoc("v1", new OpenApiInfo
+				{
+					Title = "CarBazaarApi",
+					Version = "v1"
+				});
+
+				options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+				{
+					Type = SecuritySchemeType.ApiKey,
+					Name = "Authorization",
+					In = ParameterLocation.Header,
+					Scheme = "Bearer",
+					BearerFormat = "JWT"
+				});
+
+				options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+				{
+					{
+						new OpenApiSecurityScheme
+						{
+							Reference = new OpenApiReference
+							{
+								Type = ReferenceType.SecurityScheme,
+								Id = "Bearer"
+							}
+						},
+						System.Array.Empty<string>()
+					}
+				});
+			});
+			return services;
+		}
+	}
+}

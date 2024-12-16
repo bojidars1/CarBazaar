0076daf28b28056558ac57fb293ac21e79658842
BojidarS <pichbg124@gmail.com>
2024-12-16 15:33:54 +0200

    Added Db Migration for Notifications

--- CarBazaar.Data/Migrations/20241216133334_Notification_Entity_Added.Designer.cs
+++ CarBazaar.Data/Migrations/20241216133334_Notification_Entity_Added.Designer.cs
@@ -4,6 +4,7 @@ using CarBazaar.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarBazaar.Data.Migrations
 {
     [DbContext(typeof(CarBazaarDbContext))]
-    partial class CarBazaarDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241216133334_Notification_Entity_Added")]
+    partial class Notification_Entity_Added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

b5b243b0e5398be10a19e19cc73107c1af7b22e3
BojidarS <pichbg124@gmail.com>
2024-12-16 22:56:56 +0200

    Added DbContextHelper

---
+++ CarBazaar.UnitTests/Common/DbContextHelper.cs
@@ -0,0 +1,17 @@
+﻿using CarBazaar.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarBazaar.UnitTests.Common
+{
+	public static class DbContextHelper
+	{
+		public static CarBazaarDbContext GetInMemoryDbContext(string dbName)
+		{
+			var options = new DbContextOptionsBuilder<CarBazaarDbContext>()
+				.UseInMemoryDatabase(dbName)
+				.Options;
+
+			return new CarBazaarDbContext(options);
+		}
+	}
+}

8c1240700bfa4ece42ecf523c22f86b9409cf0b6
BojidarS <pichbg124@gmail.com>
2024-12-16 13:57:38 +0200

    Added Notification entity

---
+++ CarBazaar.Data.Models/Notification.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.Data.Models
+{
+	public class Notification
+	{
+		[Key]
+		[Comment("Notification identifier")]
+		public Guid Id { get; set; } = Guid.NewGuid();
+
+		[Comment("User Id")]
+		public string UserId { get; set; } = null!;
+
+		[ForeignKey(nameof(UserId))]
+		[Comment("The user that receives the notification")]
+		public CarBazaarUser User { get; set; } = null!;
+
+		[Comment("The Notificaiton Message")]
+		public string Message { get; set; } = null!;
+
+		public bool isRead { get; set; } = false;
+
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+	}
+}

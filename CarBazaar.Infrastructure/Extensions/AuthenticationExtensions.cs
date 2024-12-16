3b298d52e85a16dfb2ec40da4f11b7aeff43bca6
BojidarS <pichbg124@gmail.com>
2024-12-16 20:26:58 +0200

    Added AuthenticationExtensions

---
+++ CarBazaar.Infrastructure/Extensions/AuthenticationExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
+namespace CarBazaar.Infrastructure.Extensions
+{
+	public static class AuthenticationExtensions
+	{
+		public static IServiceCollection AddAuthenticationConfig(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					options.RequireHttpsMetadata = false;
+					options.SaveToken = true;
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuer = true,
+						ValidateAudience = true,
+						ValidateLifetime = true,
+						ValidateIssuerSigningKey = true,
+						ClockSkew = TimeSpan.Zero,
+						ValidIssuer = configuration["JWT:Issuer"],
+						ValidAudience = configuration["JWT:Audience"],
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"])),
+					};
+				});
+			return services;
+		}
+	}
+}

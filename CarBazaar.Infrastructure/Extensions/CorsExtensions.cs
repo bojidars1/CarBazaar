307ab026b65d76a5f97977ab46e40bf68894ee9f
BojidarS <pichbg124@gmail.com>
2024-12-16 20:28:41 +0200

    Added CorsExtensions

---
+++ CarBazaar.Infrastructure/Extensions/CorsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CarBazaar.Infrastructure.Extensions
+{
+	public static class CorsExtensions
+	{
+		public static IServiceCollection AddCorsConfig(this IServiceCollection services)
+		{
+			services.AddCors(options =>
+			{
+				options.AddPolicy(name: "MySpecificOrigins", builder =>
+				{
+					builder.WithOrigins("https://localhost", "https://localhost:5173")
+						.AllowCredentials()
+						.AllowAnyMethod()
+						.AllowAnyHeader()
+						.SetIsOriginAllowedToAllowWildcardSubdomains();
+				});
+			});
+			return services;
+		}
+	}
+}

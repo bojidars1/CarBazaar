5618cc80c6d3a58cebe2d4ff3f6bdf65fe975ec9
BojidarS <pichbg124@gmail.com>
2024-12-16 20:23:59 +0200

    Added DatabaseExtensions

---
+++ CarBazaar.Infrastructure/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,19 @@
+﻿using CarBazaar.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarBazaar.Infrastructure.Extensions
+{
+	public static class DatabaseExtensions
+	{
+		public static IServiceCollection AddDatabaseConfig(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddDbContext<CarBazaarDbContext>(options =>
+			{
+				options.UseSqlServer(configuration.GetConnectionString("CarBazaarDbContext"));
+			});
+			return services;
+		}
+	}
+}

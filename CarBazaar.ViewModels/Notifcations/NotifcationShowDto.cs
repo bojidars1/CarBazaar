69a8d529b2cf9e1e82a94d3be8020d0785a73e20
BojidarS <pichbg124@gmail.com>
2024-12-16 15:07:06 +0200

    Added NotificationShowDto

---
+++ CarBazaar.ViewModels/Notifcations/NotifcationShowDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.ViewModels.Notifcations
+{
+	public class NotifcationShowDto
+	{
+		public string Id { get; set; } = null!;
+
+		public string Message { get; set; } = null!;
+
+		public bool isRead { get; set; } = false;
+	}
+}

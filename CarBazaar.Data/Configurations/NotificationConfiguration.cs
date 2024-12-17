d76985720f23c44c7452d143759e85b82d30955d
BojidarS <pichbg124@gmail.com>
2024-12-17 18:48:14 +0200

    Added NotificationConfiguration

---
+++ CarBazaar.Data/Configurations/NotificationConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using CarBazaar.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace CarBazaar.Data.Configurations
+{
+	public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+	{
+		public void Configure(EntityTypeBuilder<Notification> builder)
+		{
+			builder.HasKey(n => n.Id);
+
+			builder.HasOne(n => n.User)
+				.WithMany()
+				.HasForeignKey(n => n.UserId)
+				.OnDelete(DeleteBehavior.Restrict);
+
+			builder.HasOne(n => n.Sender)
+				.WithMany()
+				.HasForeignKey(n => n.SenderId)
+				.OnDelete(DeleteBehavior.Restrict);
+
+			builder.HasOne(n => n.CarListing)
+				.WithMany()
+				.HasForeignKey(n => n.CarListingId)
+				.OnDelete(DeleteBehavior.Cascade);
+		}
+	}
+}

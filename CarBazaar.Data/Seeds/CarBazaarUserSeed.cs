2ff17fafa8d4413d1a243e2ed57d0b06c87e0621
BojidarS <pichbg124@gmail.com>
2024-12-17 19:00:12 +0200

    Added CarBazaarUserSeed

---
+++ CarBazaar.Data/Seeds/CarBazaarUserSeed.cs
@@ -0,0 +1,32 @@
+﻿using CarBazaar.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarBazaar.Data.Seeds
+{
+	public class CarBazaarUserSeed : IEntityTypeConfiguration<CarBazaarUser>
+	{
+		public void Configure(EntityTypeBuilder<CarBazaarUser> builder)
+		{
+			var user1 = new CarBazaarUser
+			{
+				Id = Guid.NewGuid().ToString(),
+				UserName = "bojidar.stoi@gmail.com",
+				NormalizedUserName = "BOJIDAR.STOI@GMAIL.COM",
+				Email = "bojidar.stoi@gmail.com",
+				NormalizedEmail = "BOJIDAR.STOI@GMAIL.COM"
+			};
+
+			var user2 = new CarBazaarUser
+			{
+				Id = Guid.NewGuid().ToString(),
+				UserName = "john.wick@abv.bg",
+				NormalizedUserName = "JOHN.WICK@ABV.BG",
+				Email = "john.wick@abv.bg",
+				NormalizedEmail = "JOHN.WICK@ABV.BG"
+			};
+
+			builder.HasData(user1, user2);
+		}
+	}
+}

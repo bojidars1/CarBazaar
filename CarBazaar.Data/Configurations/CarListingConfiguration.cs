e39f7cf556e43966b06b71c7a7ac180fc64e8ff7
BojidarS <pichbg124@gmail.com>
2024-12-17 18:41:07 +0200

    Added CarListingConfiguration

---
+++ CarBazaar.Data/Configurations/CarListingConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using CarBazaar.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBazaar.Data.Configurations
+{
+	public class CarListingConfiguration : IEntityTypeConfiguration<CarListing>
+	{
+		public void Configure(EntityTypeBuilder<CarListing> builder)
+		{
+			builder.HasKey(cl => cl.Id);
+
+			builder.HasOne(cl => cl.Seller)
+				   .WithMany()
+				   .HasForeignKey(cl => cl.SellerId)
+				   .OnDelete(DeleteBehavior.Restrict);
+
+			builder.HasQueryFilter(cl => !cl.IsDeleted);
+		}
+	}
+}

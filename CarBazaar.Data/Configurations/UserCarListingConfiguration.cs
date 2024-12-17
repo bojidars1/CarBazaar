c8949deb234bd569c1f8917a6c8d6ab9d37fd7a3
BojidarS <pichbg124@gmail.com>
2024-12-17 18:42:11 +0200

    Added UserCarListingConfiguration

---
+++ CarBazaar.Data/Configurations/UserCarListingConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using CarBazaar.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarBazaar.Data.Configurations
+{
+	public class UserCarListingConfiguration : IEntityTypeConfiguration<UserCarListing>
+	{
+		public void Configure(EntityTypeBuilder<UserCarListing> builder)
+		{
+			builder.HasKey(uc => new { uc.UserId, uc.CarListingId });
+
+			builder.HasOne(uc => uc.User)
+				   .WithMany()
+				   .HasForeignKey(uc => uc.UserId);
+
+			builder.HasOne(uc => uc.CarListing)
+				   .WithMany()
+				   .HasForeignKey(uc => uc.CarListingId);
+		}
+	}
+}

338f516726891267fbd7303ea6558bcce224020b
BojidarS <pichbg124@gmail.com>
2024-12-17 19:05:59 +0200

    Added 1 more CarListing to CarListingSeed

--- CarBazaar.Data/Seeds/CarListingSeed.cs
+++ CarBazaar.Data/Seeds/CarListingSeed.cs
@@ -8,25 +8,43 @@ namespace CarBazaar.Data.Seeds
 	{
 		public void Configure(EntityTypeBuilder<CarListing> builder)
 		{
-			builder.HasData(
-				new CarListing
-				{
-					Id = Guid.NewGuid(),
-					SellerId = "user-1",
-					Name = "Toyota Corolla",
-					Type = "Sedan",
-					Brand = "Toyota",
-					Price = 15000,
-					Gearbox = "Automatic",
-					State = "Used",
-					Km = 80000,
-					ProductionYear = 2018,
-					Horsepower = 130,
-					Color = "White",
-					ExtraInfo = "Well maintained, low mileage.",
-					ImageURL = "https://scene7.toyota.eu/is/image/toyotaeurope/cors0005a_web_2023:Medium-Landscape?ts=1708962012070&resMode=sharp2&op_usm=1.75,0.3,2,0",
-				}
-			);
+			var carListing1 = new CarListing
+			{
+				Id = Guid.NewGuid(),
+				SellerId = "user-1",
+				Name = "Toyota Corolla",
+				Type = "Sedan",
+				Brand = "Toyota",
+				Price = 15000,
+				Gearbox = "Automatic",
+				State = "Used",
+				Km = 80000,
+				ProductionYear = 2018,
+				Horsepower = 130,
+				Color = "White",
+				ExtraInfo = "Well maintained, low mileage.",
+				ImageURL = "https://scene7.toyota.eu/is/image/toyotaeurope/cors0005a_web_2023:Medium-Landscape?ts=1708962012070&resMode=sharp2&op_usm=1.75,0.3,2,0",
+			};
+
+			var carListing2 = new CarListing
+			{
+				Id = Guid.NewGuid(),
+				SellerId = "user-2",
+				Name = "BMW X5",
+				Type = "SUV",
+				Brand = "BMW",
+				Price = 50000,
+				Gearbox = "Automatic",
+				State = "New",
+				Km = 5000,
+				ProductionYear = 2022,
+				Horsepower = 300,
+				Color = "Black",
+				ExtraInfo = "Luxury package included.",
+				ImageURL = "https://media.ed.edmunds-media.com/bmw/x5/2025/oem/2025_bmw_x5_4dr-suv_xdrive40i_fq_oem_1_1280.jpg"
+			};
+
+			builder.HasData(carListing1, carListing2);
 		}
 	}
 }

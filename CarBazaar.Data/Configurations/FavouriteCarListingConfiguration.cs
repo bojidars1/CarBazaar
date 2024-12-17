bad48c52ff501929ccf6e77384a22032a5dbaeef
BojidarS <pichbg124@gmail.com>
2024-12-17 18:44:37 +0200

    Added FavouriteCarListingConfiguration

---
+++ CarBazaar.Data/Configurations/FavouriteCarListingConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CarBazaar.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace CarBazaar.Data.Configurations
+{
+	public class FavouriteCarListingConfiguration : IEntityTypeConfiguration<FavouriteCarListing>
+	{
+		public void Configure(EntityTypeBuilder<FavouriteCarListing> builder)
+		{
+			builder.HasKey(fc => new { fc.UserId, fc.CarListingId });
+
+			builder.HasOne(fc => fc.User)
+				.WithMany()
+				.HasForeignKey(fc => fc.UserId);
+
+			builder.HasOne(fc => fc.CarListing)
+				.WithMany()
+				.HasForeignKey(fc => fc.CarListingId);
+		}
+	}
+}

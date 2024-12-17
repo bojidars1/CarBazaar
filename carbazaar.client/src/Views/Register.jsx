a9a32a020c8c356f60cc6e4239d9f26f6fbb3ab9
BojidarS <pichbg124@gmail.com>
2024-12-17 21:53:53 +0200

    Added RegisterScheme to Register

--- carbazaar.client/src/Views/Register.jsx
+++ carbazaar.client/src/Views/Register.jsx
@@ -1,116 +1,152 @@
+import React from "react";
 import { Container, Box, Typography, TextField, Button, Alert } from "@mui/material";
-import React, { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from 'react-redux';
-import { setUser } from '../redux/userSlice';
-import { setAuthenticated } from '../redux/authSlice';
+import { setUser } from "../redux/userSlice";
+import { setAuthenticated } from "../redux/authSlice";
 import { jwtDecode } from "jwt-decode";
 import api from "../api/api";
 
+const RegisterSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password")], "Passwords must match")
+    .required("Confirm Password is required"),
+});
+
 const Register = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [error, setError] = useState('');
+  const handleRegister = async (values, { setSubmitting, setErrors }) => {
+    try {
+      const response = await api.post("/account/register", {
+        email: values.email,
+        password: values.password,
+      });
 
-    const handleRegister = async (e) => {
-        e.preventDefault();
-        setError('');
+      const { accessToken } = response.data;
 
-        if (password !== confirmPassword) {
-            setError('Passwords does not match!');
-            return;
-        }
+      const decodedToken = jwtDecode(accessToken);
+      const user = {
+        userId: decodedToken.sub,
+        userEmail: decodedToken.email,
+        carListings: decodedToken.CarListings,
+      };
 
-        try {
-            const response = await api.post('/account/register', { email, password });
-            const { accessToken } = response.data;
+      localStorage.setItem("token", accessToken);
+      dispatch(setAuthenticated(true));
+      dispatch(setUser(user));
 
-            const decodedToken = jwtDecode(accessToken);
-            const userId = decodedToken.sub;
-            const userEmail = decodedToken.email;
-            const carListings = decodedToken.CarListings;
-            const user = {
-              userId: userId,
-              userEmail: userEmail,
-              carListings: carListings
-            };
-
-            localStorage.setItem('token', accessToken);
-            
-            dispatch(setAuthenticated(true));
-            dispatch(setUser(user));
-            navigate('/');
-        } catch (err) {
-            console.error(err);
-        }
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+      if (err.response && err.response.data) {
+        setErrors({ general: err.response.data.message || "Registration failed" });
+      } else {
+        setErrors({ general: "Something went wrong. Please try again later." });
+      }
+    } finally {
+      setSubmitting(false);
     }
+  };
 
-    return (
-        <Container maxWidth='xs'>
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                marginTop: 8,
-                padding: 3,
-                borderRadius: 2,
-                boxShadow: 2,
-                bgcolor: 'background.paper'
-            }}>
-                <Typography variant='h4' sx={{ mb: 4 }}>
-                    Register
-                </Typography>
+  return (
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 8,
+          padding: 3,
+          borderRadius: 2,
+          boxShadow: 2,
+          bgcolor: "background.paper",
+        }}
+      >
+        <Typography variant="h4" sx={{ mb: 4 }}>
+          Register
+        </Typography>
 
-                {error && <Alert severity='error' sx={{ mb: 2 }}>{error}</Alert>}
+        <Formik
+          initialValues={{ email: "", password: "", confirmPassword: "" }}
+          validationSchema={RegisterSchema}
+          onSubmit={handleRegister}
+        >
+          {({ errors, touched, isSubmitting }) => (
+            <Form style={{ width: "100%" }}>
+              {errors.general && (
+                <Alert severity="error" sx={{ mb: 2 }}>
+                  {errors.general}
+                </Alert>
+              )}
 
-                <form onSubmit={handleRegister} style={{ width: '100%' }}>
-                    <TextField
-                    label='Email'
-                    type='email'
-                    value={email}
-                    fullWidth
-                    required
-                    onChange={(e) => setEmail(e.target.value)}
-                    sx={{ mb: 2 }}
-                    />
+              <Field
+                as={TextField}
+                label="Email"
+                name="email"
+                type="email"
+                fullWidth
+                required
+                error={touched.email && Boolean(errors.email)}
+                helperText={touched.email && errors.email}
+                sx={{ mb: 2 }}
+              />
 
-                    <TextField
-                    label='Password'
-                    type='password'
-                    value={password}
-                    fullWidth
-                    required
-                    onChange={(e) => setPassword(e.target.value)}
-                    sx={{ mb: 2 }}
-                    />
+              <Field
+                as={TextField}
+                label="Password"
+                name="password"
+                type="password"
+                fullWidth
+                required
+                error={touched.password && Boolean(errors.password)}
+                helperText={touched.password && errors.password}
+                sx={{ mb: 2 }}
+              />
 
-                    <TextField
-                    label='Confirm Password'
-                    type='password'
-                    value={confirmPassword}
-                    fullWidth
-                    required
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    sx={{ mb: 2 }}
-                    />
+              <Field
+                as={TextField}
+                label="Confirm Password"
+                name="confirmPassword"
+                type="password"
+                fullWidth
+                required
+                error={touched.confirmPassword && Boolean(errors.confirmPassword)}
+                helperText={touched.confirmPassword && errors.confirmPassword}
+                sx={{ mb: 2 }}
+              />
 
-                    <Button type='submit' variant='contained' color='primary' fullWidth>
-                        Register
-                    </Button>
-                </form>
-                <Typography variant='body2' sx={{ mt: 2 }}>
-                    Already have an account?
-                    <Button variant='text' color='primary'>
-                        Sign In
-                    </Button>
-                </Typography>
-            </Box>
-        </Container>
-    );
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? "Registering..." : "Register"}
+              </Button>
+            </Form>
+          )}
+        </Formik>
+
+        <Typography variant="body2" sx={{ mt: 2 }}>
+          Already have an account?
+          <Button variant="text" color="primary" onClick={() => navigate("/login")}>
+            Sign In
+          </Button>
+        </Typography>
+      </Box>
+    </Container>
+  );
 };
 
 export default Register;
